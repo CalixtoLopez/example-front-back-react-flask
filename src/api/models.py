579dc388296bcc02e95c527edda52d42d08923f2
Calixto López Santos <calixto.lopez.santos@gmail.com>
2021-08-28 20:21:40 +0000

    Create BD

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import VARCHAR  # como queremos utilizar VARCHAR estamos importando un dialecto de postgres por la forma que esta estructurado Alchemy
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Account(db.Model):
+    __tablename__ = 'account'
+    #como hemos cambiado el nombre de la base de datos nos tenemos que ir al archivo admin.py and route.py
+    #vamos a utilizar VARCHAR en la postgres almacena el tamaño del string o int en la bd
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.VARCHAR, unique=True, nullable=False)
+    money = db.Column(db.Numeric, nullable=False, default=0)
+    _password = db.Column(db.VARCHAR, unique=False, nullable=False) #ponemos _password para saber que es privada 
+    _is_active = db.Column(db.Boolean, unique=False, nullable=False, default = True)
+    ##como he cambiado las columnas necetamos hacer una migración  $pipenv run migrate
+    ##tenemos la declaracion de las foreikey
+    have_client = db.relationship("Client", backref="account", lazy=True) #enumeramos las realaciones con verbos(Client se refiere a la clase, account en minuscula se refierea la tabla)
+    have_business = db.relationship("Business", backref="account", lazy=True) #enumeramos las realaciones con verbos(Business se refiere a la clase, account en minuscula se refierea la tabla)
+    ## Con este tipo de tablas aplicamos un boolenao para decirle si es un cliente 
+    is_client = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
+        return f'Account{self.id}, {self.email}' #nueva formula para devolver los datos
+    #transformamos toda esa información de la base de datos en un diccionario
+    def to_dict(self):
         return {
             "id": self.id,
             "email": self.email,
+            "money" : self.money
             # do not serialize the password, its a security breach
         }
+
+#La ForeignKey va en los hijos
+class Client(db.Model):
+    __tablename__ = 'client'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nick = db.Column(db.VARCHAR, unique=True, nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    ## La realación con el padre
+    is_account = db.relationship("Account", backref="client")
+
+class Business(db.Model):
+    __tablename__ = 'business'
+
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.VARCHAR, unique=False, nullable=False)
+    cif = db.Column(db.VARCHAR, unique=True, nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id')) # no pasa nada que las dos ForeignKey se llamen igual
+    ## La relacion con el padre
+    is_account  = db.relationship("Account", backref="business")
+
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+#tenemos que importar la tabla de la base de datos
+from api.models import db, Account
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +17,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+## Los endpoints con las rutas a las que hago las llamadas
+## Si queremos una cuenta no creamos el POST de una cuenta
+## En este caso podemos poner account pero es mas sencilo con '/client' or '/business'
+@api.route('/client', methods=['POST'])
+def create_client():
+    # Lo que tenemos que hacer es lo que nos envia el body es dicer el fecth
+    # Aplicamos es esta propiedad de python a, b = 1, 2
+    email = request.json.get('email',None)
+    money = request.json.get('money',None)
+    password = request.json.get('password',None)
+    nick = request.json.get('nick',None)
+
+    print({email} {money} {password} {nick})
+    #siempre tenemos que tener un return vacio para que no dar problemas
+    return {}, 200
